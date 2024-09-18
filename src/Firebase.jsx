1d94fabf77139856f1489d3ca03bcdb5870f9a39
Shivam yadav <shivamkumaryadav7024@gmail.com>
2024-09-19 02:31:57 +0530

    env

--- src/Firebase.jsx
+++ src/Firebase.jsx
@@ -1,16 +1,17 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
+// Use process.env to access environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyAW9wE1YSPl3g2cWA7tfpsUi_p0yUeRotY",
-  authDomain: "blog-ceea9.firebaseapp.com",
-  projectId: "blog-ceea9",
-  storageBucket: "blog-ceea9.appspot.com",
-  messagingSenderId: "51719191844",
-  appId: "1:51719191844:web:f6c542e91b823f4bf847b1"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
- const app = initializeApp(firebaseConfig);
- const auth = getAuth(app)
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
- export {auth}
+export { auth };
